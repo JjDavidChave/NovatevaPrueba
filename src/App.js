740c6fc5b17e8cb938b26bbcce9cad3f1717cde0
JjDavidChave <lianclevenord@gmail.com>
2022-10-25 11:23:15 -0500

    novateva codigo prueba

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import { Typography, Stack } from "@mui/material";
+import Select from "./Select";
+import Button from "@mui/material/Button";
+import Atribute from "./Atibute";
 
-function App() {
+function ValidationTextFields() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box
+      component="form"
+      sx={{
+        "& .MuiTextField-root": { m: 1, width: "25ch" },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <Stack direction="row" spacing={2}>
+        <Typography variant="h3">Nueva Tabla</Typography>
+        <Button variant="contained">Guardar</Button>
+      </Stack>
+      <div>
+        <TextField
+          error
+          id="outlined-error"
+          label="Nombre"
+          defaultValue="Person"
+        />
+        <TextField
+          error
+          id="outlined-error-helper-text"
+          label="Descripcion"
+          defaultValue="Tabla usada para x y z"
+        />
+      </div>
+      <div>
+        <Typography variant="h3">Atributos</Typography>
+      </div>
+
+      <div>
+        <Atribute />
+      </div>
+
+      <div>
+        <Typography variant="h3">Relaciones</Typography>
+      </div>
+
+      <div>
+        <Select />
+      </div>
+      <div>
+        <Button variant="contained" disableElevation>
+          Agregar +
+        </Button>
+      </div>
+    </Box>
   );
 }
 
-export default App;
+export default ValidationTextFields;
--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,90 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+
+
+/* import '@testing-library/jest-dom';
+
+
+
+
+import React, { useEffect, useState } from 'react'
+
+
+
+const App = () => {
+
+  const [posts, setPosts] = useState(null);
+
+useEffect(() => {
+  fetch("https://jsonplaceholder.typicode.com/posts").then(res =>{
+  return res.json()  
+  }).then(res =>{
+    console.log(res)
+    setPosts(res)
+  })
+
+}, [])
+
+
+  return (
+    <div>
+      {posts && posts.map(post => (
+        <div key={post.id}>
+          <h1 style={singStyle}>{post.id}</h1>
+          <h2 style={twoStyle}>{post.title}</h2>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+const singStyle = {
+  color: 'yellow',
+  alignItems: 'center',
+  
+}
+
+const twoStyle = {
+  color: 'blue',
+}
+
+export default App */
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+
+import React from "react";
+import Home from "./routes/Home";
+import Products from './routes/Products'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const Rutas=()=> {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/products" element={<Products />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Rutas; */
+
+
